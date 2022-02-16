17f4132e997878ec8027de7f6942f40122f5c24a
Shrey Dhiman <39773350+ShreyDhiman@users.noreply.github.com>
2022-02-16 10:40:07 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,20 +1,21 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-// const addNote = (title, body) => console.log(title, body)
-//     const notes = loadNotes()
-//     const duplicateNote = notes.find((note) => note.title === title)
-
-//     if (!duplicateNote) {
-//         notes.push({
-//             title: title,
-//             body: body
-//         })
-//         saveNotes(notes)
-//         console.log(chalk.green.inverse('New note added!'))
-//     } else {
-//         console.log(chalk.red.inverse('Note title taken!'))
-//     }
+ const addNote = (title, body) => {
+     console.log(title, body)
+     const notes = loadNotes()
+     const duplicateNote = notes.find((note) => note.title === title)
+     if (!duplicateNote) {
+         notes.push({
+             title: title,
+             body: body
+         })
+         saveNotes(notes)
+         console.log(chalk.green.inverse('New note added!'))
+     } else {
+         console.log(chalk.red.inverse('Note title taken!'))
+     }
+ }
 
 const removeNote = (title) => {
     const notes = loadNotes()
